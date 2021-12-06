8cc3ef1ba1e8d448829c55cf5a2b6c426c493e3b
meela <meelapradhan400@gmail.com>
2021-12-06 13:34:40 -0800

    FIXED BUG

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -4,7 +4,7 @@ UPDATE_DNS_RECORDS() {
    IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
    ## xargs is used to remove the double  quotes
    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-   aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq  &>/dev/null
+   aws route53 change-resource-record-sets --hosted-zone-id Z025991117KJKQB4PF332 --change-batch file:///tmp/record.json | jq  &>/dev/null
 }
 
 CREATE() {
